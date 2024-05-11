df5d84475a86f3976699e70dc7a151ebc6e23f03
Michael L. Thompson <mlthomps@msn.com>
2024-05-11 18:23:57 -0400

    Update __init__.py
    fixed import statements

--- tabcbm/concepts_xai/__init__.py
+++ tabcbm/concepts_xai/__init__.py
@@ -5,5 +5,5 @@ All credit due to Kazhdan et al. (arXiv:2104.06917) and to
 Espinosa Zarlenta et al. (AAAI, 2023) for the OCACE and SENN implementations.
 """
 
-from concepts_xai.evaluation import *
-from concepts_xai.methods import *
+from .evaluation import *
+from .methods import *
